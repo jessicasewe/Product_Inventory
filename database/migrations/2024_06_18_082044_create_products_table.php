fb9f979e5c2b92df80c3f04956ef4fa36200f823
jessicasewe <jessicasewe89@gmail.com>
2024-06-18 08:30:23 +0000

    new project

--- database/migrations/2024_06_18_082044_create_products_table.php
+++ database/migrations/2024_06_18_082044_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('product_name');
+            $table->string('product_name', 255);
             $table->integer('quantity_in_stock');
             $table->integer('price_per_item', 8, 2);
             $table->timestamps();
